09d88b786e946b75d1bb817202b3199d7c2ca13a
Adam Boduch <adam.boduch@gmail.com>
2011-05-02 15:16:23 -0400

    Updated package info.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from setuptools import setup, find_packages
 
 setup(name="spotsh",
-      version='0.0.1',
+      version='0.0.2',
       # python 2.6+ already has json module
       install_requires = ['simplejson', 'oauth'],
       zip_safe=False,
